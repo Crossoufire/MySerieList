8383d960c0163afd8a96413800e01c07186d0109
Crossoufire <47953962+Crossoufire@users.noreply.github.com>
2019-06-02 18:25:24 +0200

    Update merge Run.py

--- Run.py
+++ Run.py
@@ -7,11 +7,4 @@ if __name__ == "__main__":
         print("Config file not found. Exit.")
         sys.exit()
 
-    formatter = logging.Formatter("[%(asctime)s] %(levelname)s - %(message)s")
-    handler = RotatingFileHandler("MyLists/log/mylists.log", maxBytes=10000000, backupCount=5)
-    handler.setLevel(logging.DEBUG)
-    handler.setFormatter(formatter)
-    app.logger.addHandler(handler)
-    app.logger.setLevel(logging.DEBUG)
-
-    app.run(debug=True)
+app.run(debug=True)
