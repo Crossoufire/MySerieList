5e673777ce1b4700fb9064018e7fad448292f1f8
Cross <vince.delmas.38gmail.com>
2019-09-10 16:09:07 +0200

    update mymedialist isotope

--- MyLists/static/js/mymedialist.js
+++ MyLists/static/js/mymedialist.js
@@ -1,3 +1,4 @@
+
 // ----------- Change category --------------
 function changeCategory(element_id, new_category, card_id, mod_id, seas_drop_id, ep_drop_id, seas_data, media_list) {
     if (new_category === 'Watching') {
@@ -167,7 +168,7 @@ function changeCategory(element_id, new_category, card_id, mod_id, seas_drop_id,
     }
 
     $body = $("body");
-    $grid.isotope('layout');
+    $categories.isotope('layout');
     $.ajax ({
         type: "POST",
         url: "/change_element_category",
@@ -180,86 +181,61 @@ function changeCategory(element_id, new_category, card_id, mod_id, seas_drop_id,
     });
 }
 
-// ------------- Delete element -------------
-function delete_element(element_id, card_id, element_name, media_list) {
-    if (!confirm("Are you sure you want to delete this from your list?")) {
-        return false;
-    }
-
-    $("#"+card_id).remove();
+// ---------------- Refresh -----------------
+function refresh(element_id, media_list, user_name) {
     $body = $("body");
     $.ajax ({
         type: "POST",
-        url: "/delete_element",
+        url: "/refresh_single_element",
         contentType: "application/json",
-        data: JSON.stringify({ delete: element_id, element_type: media_list }),
+        data: JSON.stringify({ element_id: element_id, element_type: media_list }),
         dataType: "json",
+        beforeSend: function() { $body.addClass("loading"); },
         success: function(response) {
-            console.log("ok");
+            window.location.replace('/' + media_list + '/' + user_name);
         }
     });
 }
 
-// ------------ Show all score --------------
-function show_all_score() {
-    var show = document.getElementById('show_all');
-    var hide = document.getElementById('hide_all');
-    $('div[id^="footer_"]').attr('class', 'card-footer text-muted text-center p-0');
-    hide.className = "fas fa-eye-slash text-light m-r-5";
-    show.className = "d-none";
-    $('#show_hide').text('Hide scores');
-    $('#score_container').attr('onclick', 'hide_all_score()');
-    $('.card').attr('class', 'card bg-transparent m-l-10 m-r-10 m-b-15 m-t-20');
-}
+// ------------- Update episode ---------------
+function updateEpisode(element_id, episode, media_list) {
+    var selected_episode = episode.selectedIndex
 
-// ------------ Hide all score --------------
-function hide_all_score() {
-    var hide = document.getElementById('hide_all');
-    var show = document.getElementById('show_all');
-    $('div[id^="footer_"]').attr('class', 'd-none footer_score');
-    hide.className = "d-none";
-    show.className = "fas fa-eye text-light m-r-5";
-    $('#show_hide').text('Show scores');
-    $('#score_container').attr('onclick', 'show_all_score()');
-    $('.card').attr('class', 'card bg-transparent m-l-10 m-r-10 m-b-40 m-t-20');
-}
-
-// -------------- Show score ----------------
-function show_score(footer_id, card_id) {
-    var score = document.getElementById(footer_id);
-
-    if (score.className === "d-none footer_score"){
-        score.className = "card-footer text-muted text-center p-0";
-        $(card_id).attr('class', 'card bg-transparent m-l-10 m-r-10 m-b-15 m-t-20');
-    } else {
-        score.className = "d-none footer_score";
-        $(card_id).attr('class', 'card bg-transparent m-l-10 m-r-10 m-b-40 m-t-20');
-    }
-}
-
-// --------------- Isotope categories ------------------
-var $grid = $('.grid-iso').isotope({
-    itemSelector: '.category',
-    layoutMode: 'masonry'
-});
-
-// bind filter button click
-$('.filters-button-group').on('click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    console.log(filterValue);
-    $grid.isotope({ filter: filterValue });
-});
-
-// change the class on the selected button
-$('.filters-button-group').each(function(i, buttonGroup) {
-    var $buttonGroup = $(buttonGroup);
-    $buttonGroup.on('click', 'button', function() {
-        $buttonGroup.find('.btn-warning').addClass('btn-light');
-        $buttonGroup.find('.btn-warning').removeClass('btn-warning');
-        $(this).addClass('btn-warning');
-        $(this).removeClass('btn-light');
+    $body = $("body");
+    $.ajax ({
+        type: "POST",
+        url: "/update_element_episode",
+        contentType: "application/json",
+        data: JSON.stringify({episode: selected_episode, element_id: element_id, element_type: media_list }),
+        dataType: "json",
+        success: function(response) {
+            console.log("ok"); }
     });
-});
+}
 
+// -------------- Update season ---------------
+function updateSeason(element_id, value, seas_data, ep_drop_id, media_list) {
+    var selected_season = value.selectedIndex;
+    var episode_drop = document.getElementById(ep_drop_id);
+    var season_data = JSON.parse("[" + seas_data + "]");
+    console.log(season_data);
 
- $grid.isotope('layout');
+    episode_drop.length = 1;
+
+    for (i = 2; i <= season_data[0][selected_season]; i++) {
+        let opt = document.createElement("option");
+        opt.text = "Episode " + i;
+        episode_drop.appendChild(opt);
+    }
+
+    $body = $("body");
+    $.ajax ({
+        type: "POST",
+        url: "/update_element_season",
+        contentType: "application/json",
+        data: JSON.stringify({season: selected_season, element_id: element_id, element_type: media_list }),
+        dataType: "json",
+        success: function(response) {
+            console.log("ok"); }
+    });
+}
--- MyLists/static/js/mymedialist_movies.js
+++ MyLists/static/js/mymedialist_movies.js
@@ -1,3 +1,4 @@
+
 // ----------- Change category --------------
 function changeCategory(element_id, new_category, card_id, mod_id, media_list) {
     if (new_category === 'Completed') {
@@ -27,7 +28,7 @@ function changeCategory(element_id, new_category, card_id, mod_id, media_list) {
     }
 
     $body = $("body");
-    $grid.isotope('layout');
+    $categories.isotope('layout');
     $.ajax ({
         type: "POST",
         url: "/change_element_category",
@@ -39,88 +40,3 @@ function changeCategory(element_id, new_category, card_id, mod_id, media_list) {
         }
     });
 }
-
-// ------------- Delete element -------------
-function delete_element(element_id, card_id, element_name, media_list) {
-    if (!confirm("Are you sure you want to delete this from your list?")) {
-        return false;
-    }
-
-    $("#"+card_id).remove();
-    $body = $("body");
-    $.ajax ({
-        type: "POST",
-        url: "/delete_element",
-        contentType: "application/json",
-        data: JSON.stringify({ delete: element_id, element_type: media_list }),
-        dataType: "json",
-        success: function(response) {
-            console.log("ok");
-        }
-    });
-}
-
-// ------------ Show all score --------------
-function show_all_score() {
-    var show = document.getElementById('show_all');
-    var hide = document.getElementById('hide_all');
-    $('div[id^="footer_"]').attr('class', 'card-footer text-muted text-center p-0');
-    hide.className = "fas fa-eye-slash text-light m-r-5";
-    show.className = "d-none";
-    $('#show_hide').text('Hide scores');
-    $('#score_container').attr('onclick', 'hide_all_score()');
-    $('.card').attr('class', 'card bg-transparent m-l-10 m-r-10 m-b-15 m-t-20');
-}
-
-// ------------ Hide all score --------------
-function hide_all_score() {
-    var hide = document.getElementById('hide_all');
-    var show = document.getElementById('show_all');
-    $('div[id^="footer_"]').attr('class', 'd-none footer_score');
-    hide.className = "d-none";
-    show.className = "fas fa-eye text-light m-r-5";
-    $('#show_hide').text('Show scores');
-    $('#score_container').attr('onclick', 'show_all_score()');
-    $('.card').attr('class', 'card bg-transparent m-l-10 m-r-10 m-b-40 m-t-20');
-}
-
-// -------------- Show score ----------------
-function show_score(footer_id, card_id) {
-    var score = document.getElementById(footer_id);
-
-    if (score.className === "d-none footer_score"){
-        score.className = "card-footer text-muted text-center p-0";
-        $(card_id).attr('class', 'card bg-transparent m-l-10 m-r-10 m-b-15 m-t-20');
-    } else {
-        score.className = "d-none footer_score";
-        $(card_id).attr('class', 'card bg-transparent m-l-10 m-r-10 m-b-40 m-t-20');
-    }
-}
-
-
-// --------------- Isotope categories ------------------
-var $grid = $('.grid-iso').isotope({
-    itemSelector: '.category',
-    layoutMode: 'masonry'
-});
-
-// bind filter button click
-$('.filters-button-group').on('click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    console.log(filterValue);
-    $grid.isotope({ filter: filterValue });
-});
-
-// change the class on the selected button
-$('.filters-button-group').each(function(i, buttonGroup) {
-    var $buttonGroup = $(buttonGroup);
-    $buttonGroup.on('click', 'button', function() {
-        $buttonGroup.find('.btn-warning').addClass('btn-light');
-        $buttonGroup.find('.btn-warning').removeClass('btn-warning');
-        $(this).addClass('btn-warning');
-        $(this).removeClass('btn-light');
-    });
-});
-
-
- $grid.isotope('layout');
