c8051f58b70875b0dce30bd5135889345340c76f
Crossoufire <47953962+Crossoufire@users.noreply.github.com>
2019-09-24 18:29:48 +0200

    add badges

--- MyLists/routes.py
+++ MyLists/routes.py
@@ -2026,16 +2026,16 @@ def get_achievements_test(user_id, list_type):
                                         join(MoviesGenre, MoviesGenre.movies_id == Movies.id). \
                                         filter(MoviesList.user_id == user_id).group_by(Movies.id).order_by(Movies.name.asc())
         genre_id = ['16', '35', '99', '14', '36', '27', '10402', '9648', '10749', '878', '53']
-        """     "id": 16, "name": "Animation"
-                "id": 35, "name": "Comedy"
-                "id": 99, "name": "Documentary"
-                "id": 14, "name": "Fantasy"
-                "id": 36, "name": "History"
-                "id": 27, "name": "Horror"
-                "id": 10402, "name": "Music"
-                "id": 9648, "name": "Mystery"
-                "id": 10749, "name": "Romance"
-                "id": 878, "name": "Science Fiction"
+        """ "id": 16, "name": "Animation"
+            "id": 35, "name": "Comedy"
+            "id": 99, "name": "Documentary"
+            "id": 14, "name": "Fantasy"
+            "id": 36, "name": "History"
+            "id": 27, "name": "Horror"
+            "id": 10402, "name": "Music"
+            "id": 9648, "name": "Mystery"
+            "id": 10749, "name": "Romance"
+            "id": 878, "name": "Science Fiction"
             "id": 53, "name": "Thriller" """
         media = "M"
 
