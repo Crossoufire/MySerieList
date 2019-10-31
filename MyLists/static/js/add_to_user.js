d1813350edbe86e1fe55c4ee5d198537a21aa623
Cross <vince.delmas.38gmail.com>
2019-10-31 16:08:44 +0100

    Change the categories selection (70% done)

--- MyLists/static/js/add_to_user.js
+++ MyLists/static/js/add_to_user.js
@@ -1,5 +1,28 @@
 
-function add_user(element_id, media_type, add_cat, card_id) {
+// -------------- Charge the buttons to choose the category -------------------
+function add_user(card_id, element_id, media_type) {
+    remove_cat();
+
+    $('#'+card_id).children().prepend(
+    "<ul class='cat_buttons'>" +
+        "<li class='btn btn_sm btn-light p-1 m-1 cat_buttons_pos' onclick='add_to_user(this, \"" + card_id + "\", \"" + element_id + "\", \"" + media_type + "\")'>Watching</li>" +
+        "<li class='btn btn_sm btn-light p-1 m-1 cat_buttons_pos' onclick='add_to_user(this, \"" + card_id + "\", \"" + element_id + "\", \"" + media_type + "\")')'>Completed</li>" +
+        "<li class='btn btn_sm btn-light p-1 m-1 cat_buttons_pos' onclick='add_to_user(this, \"" + card_id + "\", \"" + element_id + "\", \"" + media_type + "\")')'>On Hold</li>" +
+        "<li class='btn btn_sm btn-light p-1 m-1 cat_buttons_pos' onclick='add_to_user(this, \"" + card_id + "\", \"" + element_id + "\", \"" + media_type + "\")')'>Random</li>" +
+        "<li class='btn btn_sm btn-light p-1 m-1 cat_buttons_pos' onclick='add_to_user(this, \"" + card_id + "\", \"" + element_id + "\", \"" + media_type + "\")')'>Dropped</li>" +
+        "<li class='btn btn_sm btn-light p-1 m-1 cat_buttons_pos' onclick='add_to_user(this, \"" + card_id + "\", \"" + element_id + "\", \"" + media_type + "\")')'>Plan to Watch</li>" +
+    "</ul>");
+
+    $('#'+card_id).children().children('.btn_bottom_left').attr('style', 'display: none;');
+    $('#'+card_id).children().children('.mask').hide();
+    $('#'+card_id).children().prepend("<a class='btn_right_2 fas fa-times' onclick='remove_cat()')></a>");
+    $('#'+card_id).children().children('.card-img-top').attr('style', 'filter: brightness(0%);');
+}
+
+function add_to_user(cat, card_id, element_id, media_type) {
+    var add_cat = cat.childNodes[0].data
+    console.log(add_cat);
+
     $body = $("body");
     $.ajax ({
         type: "POST",
@@ -11,6 +34,7 @@ function add_user(element_id, media_type, add_cat, card_id) {
             console.log("ok");
         }
     });
+    remove_cat();
     $("#"+card_id).children().append("<div class='ribbon'></div>");
     $("#"+card_id).children().children().remove(".btn_left.fas.fa-plus.text-light");
 }
